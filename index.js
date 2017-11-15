80439fda0c2c9a7d45d0ed3b6605aeffaa959a39
Ekta Sharma <ektasharma@Ektas-MacBook-Air.local>
2017-11-15 14:35:46 -0500

    Registration MongoDB model

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
 const config = require('./config/database');
-const path = require('path'); 
+const path = require('path');
+const authentication = require('./routes/authentication')(router);
+const bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/', { useMongoClient: true });
+mongoose.connect('mongodb://localhost:27017/', {
+  useMongoClient: true
+});
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client/dist/'));
+app.use('/authentication', authentication);
 
-app.get('*',(req, res) => {
-res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
 app.listen(8080, () => {
