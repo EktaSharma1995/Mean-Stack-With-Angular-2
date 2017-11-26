36bda605067e2baccf0887eed6be853aed4384dd
Ekta Sharma <ektasharma@Ektas-MacBook-Air.local>
2017-11-26 09:22:33 -0500

    like-dislike-blog

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,60 +2,63 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { RegisterComponent }  from './components/register/register.component';
-import { LoginComponent }  from './components/login/login.component';
-import { ProfileComponent }  from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PublicProfileComponent } from './components/public-profile/public-profile.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { EditBlogComponent } from './components/blog/edit-blog/edit-blog.component';
 import { DeleteBlogComponent } from './components/blog/delete-blog/delete-blog.component';
-import { AuthGuard }  from './guards/auth.guard';
-import { NotAuthGuard }  from './guards/notAuth.guard';
+import { AuthGuard } from './guards/auth.guard';
+import { NotAuthGuard } from './guards/notAuth.guard';
 
-
+// Our Array of Angular 2 Routes
 const appRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent // Default Route
   },
   {
     path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [AuthGuard]
+    component: DashboardComponent, // Dashboard Route,
+    canActivate: [AuthGuard] // User must be logged in to view this route
   },
   {
     path: 'register',
-    component: RegisterComponent,
-    canActivate: [NotAuthGuard]
+    component: RegisterComponent, // Register Route
+    canActivate: [NotAuthGuard] // User must NOT be logged in to view this route
   },
   {
     path: 'login',
-    component: LoginComponent,
-    canActivate: [NotAuthGuard]
+    component: LoginComponent, // Login Route
+    canActivate: [NotAuthGuard] // User must NOT be logged in to view this route
   },
   {
     path: 'profile',
-    component: ProfileComponent,
-    canActivate: [AuthGuard]
+    component: ProfileComponent, // Profile Route
+    canActivate: [AuthGuard] // User must be logged in to view this route
   },
   {
     path: 'blog',
-    component: BlogComponent,
-    canActivate: [AuthGuard]
+    component: BlogComponent, // Blog Route,
+    canActivate: [AuthGuard] // User must be logged in to view this route
   },
   {
     path: 'edit-blog/:id',
-    component: EditBlogComponent, // Edit Blog ROute
-    canActivate: [AuthGuard] // User must be loggedin to view this route
+    component: EditBlogComponent, // Edit Blog Route
+    canActivate: [AuthGuard] // User must be logged in to view this route
   },
   {
     path: 'delete-blog/:id',
-    component: DeleteBlogComponent, // Edit Blog ROute
-    canActivate: [AuthGuard] // User must be loggedin to view this route
+    component: DeleteBlogComponent, // Delete Blog Route
+    canActivate: [AuthGuard] // User must be logged in to view this route
   },
   {
-    path: '**',
-    component: HomeComponent
-  }
+    path: 'user/:username',
+    component: PublicProfileComponent, // Public Profile Route
+    canActivate: [AuthGuard] // User must be logged in to view this route
+  },
+  { path: '**', component: HomeComponent } // "Catch-All" Route
 ];
 
 @NgModule({
@@ -65,4 +68,5 @@ const appRoutes: Routes = [
   bootstrap: [],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
